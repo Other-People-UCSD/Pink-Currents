1fbaa64db4f95def69b6fccbd167254692be18ba
“Eunsoo <leeunsu@live.comssh-keygen -t rsa -C leeunsu@live.comcat>
2020-07-31 01:17:52 -0700

    1.18

--- _site/assets/js/scrollBackgroundColor.js
+++ _site/assets/js/scrollBackgroundColor.js
@@ -12,6 +12,9 @@
             $("header").css("color", "var(--background-color)");
             $(".posts").css("border-color", "var(--background-color)");
 
+    
+     
+
         } else {
             $("body").css("background-color", "var(--background-color)");
             $("body").css("color", "var(--light-text-color)");
@@ -20,6 +23,9 @@
             $("header").css("background-color", "var(--background-color)");
             $("header").css("color", "var(--light-text-color)");
             $(".posts").css("border-color", "var(--light-text-color)");
+
+
+      
 
         }
     });
