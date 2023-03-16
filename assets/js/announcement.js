9fef86bcdc71311fd687d8270304102b1170f99d
Kevin-Jjang <k1jang@ucsd.edu>
2023-03-15 17:15:03 -0700

    v3.3.1 (2)

--- assets/js/announcement.js
+++ assets/js/announcement.js
@@ -1,13 +1,13 @@
 localStorage = Window.localStorage;
 window.addEventListener('DOMContentLoaded', function() {
-    if(localStorage.getItem('announcement-2-10')) {
+    if(localStorage.getItem('announcement-3-15')) {
         hideAnnouncement();
     }
 });
 
 
 function hideAnnouncement() {
-    localStorage.setItem('announcement-2-10', 'true');
+    localStorage.setItem('announcement-3-15', 'true');
     announcement = document.getElementById('announcement');
     announcement.style.display = 'none';
 }
