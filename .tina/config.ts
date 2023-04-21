2b65281671dcf9804d8acb0872779fd66e758c83
Kevin-Jjang <k1jang@ucsd.edu>
2023-04-20 23:26:59 -0700

    Update Tina schema

--- .tina/config.ts
+++ .tina/config.ts
@@ -2,14 +2,15 @@ import { defineConfig } from "tinacms";
 import { homeFields } from "./templates";
 import { postFields } from "./templates";
 import { submissionsFields } from "./templates";
+import { envLocal } from "./localDev";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: process.env.TINA_PUBLIC_CLIENT_ID!, // Get this from tina.io
-  token: process.env.TINA_TOKEN!, // Get this from tina.io
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID! || envLocal.clientId, // Get this from tina.io
+  token: process.env.TINA_TOKEN! || envLocal.token, // Get this from tina.io
   client: { skip: true },
   build: {
     outputFolder: "admin",
@@ -24,19 +25,55 @@ export default defineConfig({
   schema: {
     collections: [
       {
-        format: "md",
         label: "Includes",
         name: "includes",
         path: "_includes",
-        match: {
-          include: "**/headerMain",
-        },
         fields: [
           {
             type: "rich-text",
             name: "body",
             label: "Body of Document",
             description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "post",
+        label: "Posts",
+        path: "_posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "post",
+        label: "Landing Pages",
+        path: "",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
             isBody: true,
           },
         ],
