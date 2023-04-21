a9e7c579b3a2088997e1f367263b4fb45ffc188f
Kevin-Jjang <k1jang@ucsd.edu>
2023-04-20 21:58:08 -0700

    Added environment vars to Tina config

--- tina/config.ts
+++ tina/config.ts
@@ -8,8 +8,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: null, // Get this from tina.io
-  token: null, // Get this from tina.io
+  clientId: process.env.PUBLIC_TINA_CLIENT_ID!, // Get this from tina.io
+  token: process.env.TINA_TOKEN!, // Get this from tina.io
   client: { skip: true },
   build: {
     outputFolder: "admin",
