88afd6fc8163ceda1af5c9403f453bd364ba4e4c
Other People <60672304+otherpeopleucsd@users.noreply.github.com>
2023-04-20 22:00:29 -0700

    Renamed variable to match GitHub secret

--- tina/config.ts
+++ tina/config.ts
@@ -8,7 +8,7 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: process.env.PUBLIC_TINA_CLIENT_ID!, // Get this from tina.io
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID!, // Get this from tina.io
   token: process.env.TINA_TOKEN!, // Get this from tina.io
   client: { skip: true },
   build: {
